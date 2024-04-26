dbd35ad77d7028d4da4bef2ab8356a337210330c
Ihor Avdiunin <igor.avdiunin@gmail.com>
2024-04-26 15:46:30 +0300

    changed top for input

--- src/components/Filter/Filter.styled.ts
+++ src/components/Filter/Filter.styled.ts
@@ -2,7 +2,7 @@ export const FilterBoxStyles = {
     position: "absolute",
     left: "50%",
     transform: "translateX(-50%)",
-    top: "120px",
+    top: "150px",
     display: "flex",
     justifyContent: "center",
     margin: "0 auto",
@@ -13,7 +13,7 @@ export const FilterTextFieldStyles = {
     width: "530px",
     borderRadius: "10px",
     backgroundColor: "#ffffff",
-
+    // zIndex: "1000000",
     '@media (max-width: 550px)': {
         width: "330px",
     }
